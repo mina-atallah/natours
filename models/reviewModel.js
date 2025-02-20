c8042de83511f14bede6d2845f2ed9de903be971
mina-atallah <minaatallah18@gmail.com>
2025-02-20 16:00:12 +0200

    feat(reviews): implement review routes, controller, and update model - Created review router (`reviewRoutes.js`) with protected and restricted access for getting and creating reviews. - Created review controller (`reviewController.js`) with `getAllReviews` and `createReview` methods. - Updated review model (`reviewModel.js`) to include references to `User` and `Tour` buy adding query middleware to populate both fields. - Integrated review routes into `app.js`.

--- models/reviewModel.js
+++ models/reviewModel.js
@@ -25,5 +25,17 @@ const reviewSchema = new mongoose.Schema(
   }
 );
 
+reviewSchema.pre(/^find/, function(next) {
+  // two quiries
+  this.populate({
+    path: 'tour',
+    select: 'name'
+  }).populate({
+    path: 'user',
+    select: 'name photo'
+  });
+  next();
+});
+
 const Review = mongoose.model('Review', reviewSchema);
 module.exports = Review;
