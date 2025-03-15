5f5a6f5f2ab80dadf39ce6a5ea9034027e746501
mina-atallah <minaatallah18@gmail.com>
2025-03-16 01:55:14 +0200

    WIP feat(login): add login template and create login view route - created login template - changed 'button' to 'a' in header to navigate to login route - implement getLoginForm in view route -  rendered login template in viewController

--- controllers/viewController.js
+++ controllers/viewController.js
@@ -23,3 +23,9 @@ exports.getTourView = catchAsync(async (req, res, next) => {
     tour
   });
 });
+
+exports.getLoginForm = catchAsync(async (req, res, next) => {
+  res.status(200).render('login', {
+    title: 'Log into your account'
+  });
+});
--- routes/viewRoutes.js
+++ routes/viewRoutes.js
@@ -6,5 +6,6 @@ const router = express.Router();
 
 router.get('/', viewController.getOverview);
 router.get('/tour/:slug', viewController.getTourView);
+router.get('/login', viewController.getLoginForm);
 
 module.exports = router;
